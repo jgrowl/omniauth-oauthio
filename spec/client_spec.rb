938bb3b06d83d8ba2a49b6d20fd2b4c82d7ddb92
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-08-16 20:37:54 -0400

    Add tests for Client#authorize_url

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,18 +1,28 @@
 RSpec.describe Oauthio::Client do
   let(:site) { 'https://oauth.io' }
-  subject { Oauthio::Client.new('123', '53cr37', options) }
+  let(:client) { Oauthio::Client.new('123', '53cr37', options) }
 
   describe 'me_url' do
+    let(:provider) { 'google' }
+    subject { client.me_url(provider, params) }
+
     context 'when me_url option is not specified' do
       let(:options) { {:site => site} }
 
-      it 'replaces provider in URL' do
-        expect(subject.me_url('google')).to eq("#{site}/auth/google/me")
+      context 'without URL parameters' do
+        let(:params) { {} }
+
+        it 'replaces provider in URL' do
+          expect(subject).to eq("#{site}/auth/#{provider}/me")
+        end
       end
 
-      it 'includes given URL parameters' do
-        expect(subject.me_url('twitter', {'foo' => 'bar', 'cat' => 'dog'})).
-            to eq("#{site}/auth/twitter/me?cat=dog&foo=bar")
+      context 'with URL parameters' do
+        let(:params) { {'foo' => 'bar', 'cat' => 'dog'} }
+
+        it 'includes given URL parameters' do
+          expect(subject).to eq("#{site}/auth/#{provider}/me?cat=dog&foo=bar")
+        end
       end
     end
 
@@ -21,14 +31,69 @@ RSpec.describe Oauthio::Client do
         {:site => site, :me_url => '/big-cats/catch-mice/:provider'}
       }
 
-      it 'replaces provider in URL' do
-        expect(subject.me_url('twitter')).
-            to eq("#{site}/big-cats/catch-mice/twitter")
+      context 'without URL parameters' do
+        let(:params) { {} }
+
+        it 'replaces provider in URL' do
+          expect(subject).to eq("#{site}/big-cats/catch-mice/#{provider}")
+        end
       end
 
-      it 'includes given URL parameters' do
-        expect(subject.me_url('facebook', {'foo' => 'bar', 'cat' => 'dog'})).
-            to eq("#{site}/big-cats/catch-mice/facebook?cat=dog&foo=bar")
+      context 'with URL parameters' do
+        let(:params) { {'foo' => 'bar', 'cat' => 'dog'} }
+
+        it 'includes given URL parameters' do
+          expect(subject).
+              to eq("#{site}/big-cats/catch-mice/#{provider}?cat=dog&foo=bar")
+        end
+      end
+    end
+  end
+
+  describe 'authorize_url' do
+    let(:provider) { 'twitter' }
+    subject { client.authorize_url(provider, params) }
+
+    context 'when authorize_url option is not specified' do
+      let(:options) { {:site => site} }
+
+      context 'without URL parameters' do
+        let(:params) { {} }
+
+        it 'replaces provider in URL' do
+          expect(subject).to eq("#{site}/auth/#{provider}")
+        end
+      end
+
+      context 'with URL parameters' do
+        let(:params) { {'foo' => 'bar', 'cat' => 'dog'} }
+
+        it 'includes given URL parameters' do
+          expect(subject).to eq("#{site}/auth/#{provider}?cat=dog&foo=bar")
+        end
+      end
+    end
+
+    context 'when authorize_url option is specified' do
+      let(:options) {
+        {:site => site, :authorize_url => '/:provider/FABULOUS-URL'}
+      }
+
+      context 'without URL parameters' do
+        let(:params) { {} }
+
+        it 'replaces provider in URL' do
+          expect(subject).to eq("#{site}/#{provider}/FABULOUS-URL")
+        end
+      end
+
+      context 'with URL parameters' do
+        let(:params) { {'foo1' => 'bar', 'cat' => 'dog2'} }
+
+        it 'includes given URL parameters' do
+          expect(subject).
+              to eq("#{site}/#{provider}/FABULOUS-URL?cat=dog2&foo1=bar")
+        end
       end
     end
   end
